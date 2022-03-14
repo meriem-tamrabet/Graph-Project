3c362076f6fa48336aa88e3dfcc47c26b0eb9b6d
aureliaantz <aurelia.antz@gmail.com>
2022-03-14 17:19:13 +0100

    Ajout des méthodes toString, read et move

--- Graph/src/Class/Point.java
+++ Graph/src/Class/Point.java
@@ -1,34 +1,55 @@
 package Class;
-
+import java.util.Scanner;
 public class Point {
- private double x, y ;
+	private double x, y ;
 
  
-public Point(double x, double y) {
-	
-	this.x = x;
-	this.y = y;
-}
+	public Point(double x, double y) {
+		
+		this.x = x;
+		this.y = y;
+	}
 
 
-public double getX() {
-	return x;
-}
+	public double getX() {
+		return x;
+	}
 
 
-public void setX(double x) {
-	this.x = x;
-}
+	public void setX(double x) {
+		this.x = x;
+	}
 
 
-public double getY() {
-	return y;
-}
+	public double getY() {
+		return y;
+	}
 
 
-public void setY(double y) {
-	this.y = y;
-} 
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public String toString(){
+		return "(" + x + ", " + y + ")"; 
+	}
+
+	public void move(double x, double y){
+		this.x += x;
+		this.y += y;
+	}
+
+	public void read(Scanner s){
+
+		Scanner scan = s;
+		
+		double nx = scan.nextDouble();
+		this.setX(nx);
+		
+		double ny = scan.nextDouble();
+		this.setY(ny);
+	}
+ 
  // TODO Print 
  // TODO TOSTRING 
 // TODO SHIFT AND MOVE
